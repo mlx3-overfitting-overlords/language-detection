d7646286981fe20b916a80015c7e45e5acbafeee
Daniel Dinnyes <dadinn@besenczy.net>
2023-10-23 17:44:35 +0100

    added language prediction endpoint with dummy data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import torch
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return f'Hello from Flask with PyTorch {torch.__version__}!'
+
+@app.route('/what_language_is_this')
+def what_language():
+    predictions = [
+        { 'class': 'German', 'value': 0.10 },
+        { 'class': 'Esperanto', 'value': 0.10 },
+        { 'class': 'French', 'value': 0.10 },
+        { 'class': 'Italian', 'value': 0.50 },
+        { 'class': 'Spanish', 'value': 0.05 },
+        { 'class': 'Turkish', 'value': 0.05 },
+        { 'class': 'English', 'value': 0.10 }]
+
+    return jsonify(predictions)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3031)
