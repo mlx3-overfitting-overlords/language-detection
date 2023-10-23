67616cde7c4702aef48786e50266e5bba0377a0c
jcrich1469 <james@mailmash.net>
2023-10-23 19:20:38 +0100

    resolving conflcits

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import torch
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +19,25 @@ def what_language():
         { 'class': 'English', 'value': 0.10 }]
 
     return jsonify(predictions)
+@app.route('/example-post', methods=['POST'])
+def post_example():
+    request_data = request.get_json()
+    name = request_data['Student Name']
+    course = request_data['Course']
+    python_version = request_data['Test Marks']['Mathematics']
+    example = request_data['Course Interested'][0]
+    return '''
+    The student name is: {}
+    The course applied for is: {}
+    The test marks for Mathematics is: {}
+    The Course student is interested in is: {}'''.format(name, course, python_version, example)
+
+
+
+@app.route('/example-get', methods=['GET'])
+def get_data():
+    return requests.get('http://example.com').content
+>>>>>>> 95d4db0 (added to flask get request and added requests module to dockerfile)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3031)
+    app.run(host='0.0.0.0', port=3031, debug=True)
